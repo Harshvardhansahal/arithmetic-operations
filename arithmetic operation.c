45e983e59ac5039c6f7efe685545fad448fed8a8
Thangarajibro <43046154+Thangarajibro@users.noreply.github.com>
2018-09-06 12:09:53 -0700

    Add files via upload

---
+++ arithmetic operation.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+
+int main()
+
+{
+    
+int a=20,b=10,c,d,e,f,g;
+ 
+  c=a+b;
+   
+  d=a-b;
+     
+  e=a*b;
+     
+  f=a/b;
+     
+  g=a%b;
+
+printf("additon of two numbers is:%d\n",c);
+
+printf("subtraction of two numbers is:%d\n",d);
+
+printf("multiplication of two numbers is:%d\n",e);
+
+printf("division of two numbers is:%d\n",f);
+
+printf("modulo of two numbers is:%d\n",g);
+
+return 0;
+
+}
